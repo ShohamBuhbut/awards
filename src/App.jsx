ef0c8c0949cc8ec64bcb2edfedfda68cb733b63f
Shoham <shoamshlomi2@gmail.com>
2024-12-08 22:56:44 +0200

    Added the navbar and added some animations and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Hero from './components/hero'
 import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero />
-
-      <About />
+        <Navbar />
+        <Hero />
+        <About />
     </main>
   )
 }
